44d9d6023e814d96244d1aee8d21c13e2042eedc
ZhangTino <80563920@qq.com>
2018-05-20 17:49:21 +0800

    creat repository

---
+++ balance/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  balance
+//
+//  Created by ZhangTino on 2018/5/20.
+//  Copyright © 2018年 ZhangTino. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ balance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  balance
+//
+//  Created by ZhangTino on 2018/5/20.
+//  Copyright © 2018年 ZhangTino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
